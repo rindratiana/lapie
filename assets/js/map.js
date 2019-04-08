6ea8cd42ae3b4e28252b8cec9582cb463a6842d3
unknown <rindrap16.aps1@gmail.com>
2019-04-08 18:57:12 +0300

    header ok

--- assets/js/map.js
+++ assets/js/map.js
@@ -11,10 +11,10 @@ function initialize() {
 		zoom: 18,
 		center: center_qud, //change the coordinates
 		mapTypeId: google.maps.MapTypeId.HYBRID,
-		scrollwheel: false,
-		mapTypeControl: false,
-		zoomControl: false,
-		streetViewControl: false,
+		scrollwheel: true,
+		mapTypeControl: true,
+		zoomControl: true,
+		streetViewControl: true,
 		styles: [{"elementType":"geometry","stylers":[{"color":"#f5f5f5"}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"elementType":"labels.text.fill","stylers":[{"color":"#616161"}]},{"elementType":"labels.text.stroke","stylers":[{"color":"#f5f5f5"}]},{"featureType":"administrative.land_parcel","elementType":"labels.text.fill","stylers":[{"color":"#bdbdbd"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#eeeeee"}]},{"featureType":"poi","elementType":"labels.text.fill","stylers":[{"color":"#757575"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#e5e5e5"}]},{"featureType":"poi.park","elementType":"labels.text.fill","stylers":[{"color":"#9e9e9e"}]},{"featureType":"road","elementType":"geometry","stylers":[{"color":"#ffffff"}]},{"featureType":"road.arterial","elementType":"labels.text.fill","stylers":[{"color":"#757575"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#dadada"}]},{"featureType":"road.highway","elementType":"labels.text.fill","stylers":[{"color":"#616161"}]},{"featureType":"road.local","elementType":"labels.text.fill","stylers":[{"color":"#9e9e9e"}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"color":"#e5e5e5"}]},{"featureType":"transit.station","elementType":"geometry","stylers":[{"color":"#eeeeee"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#c9c9c9"}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"color":"#9e9e9e"}]}]
 		}
 	var img_icon = 'img/map-marker.png'
@@ -22,6 +22,7 @@ function initialize() {
 	var marker = new google.maps.Marker({
 		map: map,
 		icon: img_icon,
+		draggable: true,
 		position: new google.maps.LatLng(-18.913384,47.514174) //change the coordinates
 	});
 	google.maps.event.addListener(marker, "click", function() {
